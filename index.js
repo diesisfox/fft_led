7afc9d23bf2204caa75744e26a8f2fccf2e26f3d
diesisfox <james.zhi.liu@gmail.com>
2016-10-12 23:05:59 -0400

    Added Serial Interface
    + configured eslint
    + index.js

--- index.js
+++ index.js
@@ -1,44 +1,92 @@
-'use strict';
-const electron = require('electron');
+const SerialPort = require('serialport');
 
-const app = electron.app;
+var comPorts, serial;
 
-// adds debug features like hotkeys for triggering dev tools and reload
-require('electron-debug')();
+document.onload = () => {
+	scanPorts();
+};
 
-// prevent window being garbage collected
-let mainWindow;
+function scanPorts(){
+	document.getElementById('connect').disabled = true;
+	document.getElementById('connect').innerHTML = 'Connect';
+	document.getElementById('scan').disabled = false;
+	document.getElementById('serialSelect').disabled = false;
+	SerialPort.list((err, ports) => {
+		if(err){
+			console.log(err);
+		}else{
+			let list = [];
+			if(ports.length){
+				for(let item of ports){
+					list.push(item.comName);
+				}
+			}
+			comPorts = list;
+			updatePortsSelect(comPorts);
+		}
+	});
+};
 
-function onClosed() {
-	// dereference the window
-	// for multiple windows store them in an array
-	mainWindow = null;
-}
+function updatePortsSelect(list){ //takes an array of comNames
+	let inner = '';
+	for(var item of list){
+		inner += '<option value="' + item + '">' + item + '</option>\n';
+	}
+	console.log(inner);
+	document.getElementById('serialSelect').innerHTML = inner;
+	if(list.length){
+		document.getElementById('connect').disabled = false;
+	}
+};
 
-function createMainWindow() {
-	const win = new electron.BrowserWindow({
-		width: 600,
-		height: 400
+function connectSerial(){
+	if(serial && serial.isOpen()){
+		disconnectSerial();
+		return;
+	}
+	let s = document.getElementById('serialSelect');
+	serial = new SerialPort(s.options[s.selectedIndex].value, {
+		baudRate: 115200
+	});
+	setupSerial();
+	s.disabled = true;
+	document.getElementById('connect').disabled = true;
+	document.getElementById('connect').innerHTML = 'Connecting...';
+	document.getElementById('scan').disabled = true;
+};
+
+function setupSerial(){
+	serial.on('open', () => {
+		console.log('serial opened');
+		document.getElementById('connect').disabled = false;
+		document.getElementById('connect').innerHTML = 'Disconnect';
 	});
 
-	win.loadURL(`file://${__dirname}/index.html`);
-	win.on('closed', onClosed);
+	serial.on('error', (e) => {
+		if(serial.isOpen()){
+			serial.close((e) => {
+				console.log('error: ' + e);
+			});
+		}
+	});
 
-	return win;
+	serial.on('disconnect', (e) => {
+		console.log('serial disconnected');
+		if(e) console.log(e);
+	});
+
+	serial.on('close', (e) => {
+		console.log('serial closed');
+		if(e) console.log(e);
+		scanPorts();
+		serial = null;
+	});
+
+	serial.on('data', (data) => {
+		console.log('data: ' + data);
+	});
 }
 
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
-
-app.on('activate', () => {
-	if (!mainWindow) {
-		mainWindow = createMainWindow();
-	}
-});
-
-app.on('ready', () => {
-	mainWindow = createMainWindow();
-});
+function disconnectSerial(){
+	serial.close();
+}
